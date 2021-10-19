3290ce9efa1eb7691ebc8b2fd5cd636b749948b2
Vladymyr Shevchuk <dosandkv@gmail.com>
2021-10-19 22:38:31 +0300

    Added modules #7,8,9,10

---
+++ 10-webpack/webpack.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const obj = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    app: path.join(__dirname, './index.js')
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, './dist'),
+  }
+};
+
+module.exports = obj
